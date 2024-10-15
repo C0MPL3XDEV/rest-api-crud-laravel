a6894e2d79b6dbe33e9c5e162bec1302551ee286
C0MPL3XDEV <complexdev3@gmail.com>
2024-10-15 17:22:25 +0200

    Update - Created the `store` method for the API

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
+    // Method to get the list of all records in the DB
     public function index() {
+
         $products = Product::get();
 
         if (count($products) > 0) {
@@ -17,5 +20,36 @@ class ProductController extends Controller
         } else {
             return response()->json(['message'=> 'No Products Available'], 200);
         }
+    }
+
+    // Method to create a new record in the DB
+    public function store(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required | string | max:255',
+            'description' => 'required | string | max:255',
+            'price' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => "All fields are mandatory",
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        // Execute the query to create the new record
+
+        $product = Product::create([
+           'name' => $request->name,
+           'description' => $request->description,
+           'price' => $request->price,
+        ]);
+
+        return response()->json([
+            'message' => "New Product Added Successfully",
+            'data' => new ProductResource($product)
+        ], 200);
+
     }
 }
